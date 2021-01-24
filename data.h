46ab15fc7543c776ca27597c04a4548a11f5a815
syekohh <54860722+syekohh@users.noreply.github.com>
2021-01-24 15:39:52 -0500

    Fix?

--- data.h
+++ data.h
@@ -2,9 +2,9 @@
 #define upArrow 11
 #define mouseClick 12
 
-#define upVal 99
-#define downVal 97
-#define clickVal 95
+#define upVal "99"
+#define downVal "97"
+#define clickVal "95"
 
 #define xInput A0
 #define yInput A1
